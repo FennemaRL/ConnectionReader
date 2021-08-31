2155ed9bbebc388294a6b7e4dd8ff50007549d96
Fennema lucas <ld.fennema@gmail.com>
2021-08-30 22:20:14 -0300

    update 1, change to tokio async thread imp

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,97 @@
 use core::time;
-use std::io;
+use std::time::Duration;
+use std::{fmt, io};
 use std::sync::mpsc;
 use std::thread;
 
+use bluez::Address;
+use bluez::client::BlueZClient;
+use tokio::time::sleep;
 
-fn main() {
+
+struct ShortbtData {
+    pub name: String,
+    pub short_name: String,
+
+    pub address: Address,
+    pub bluetooth_version: u8,
+}
+
+impl fmt::Display for ShortbtData {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "(name: {}, short_name: {}, address: {},bluetooth_version: {})", &self.name, &self.short_name, self.address, self.bluetooth_version)
+    }
+}
+#[tokio::main]
+async fn main() {
     let (tx, rx) = mpsc::channel();
+    tokio::spawn(async move {
 
-    thread::spawn(move || {
-        loop{
-            let mut input = String::new();
-            println!("Ingrese un valor");
-            io::stdin().read_line(&mut input).expect("Error reading from STDIN");
-            tx.send(input).unwrap();
+        let mut client = BlueZClient::new().unwrap();
+        loop  {
+            let controllers =match client.get_controller_list().await   {
+                Ok(res) => res,
+                _ => Vec::new()
+            };
+
+            println!("Ok controller info :{:#?}",&controllers);
+            for controller in controllers {
+
+                match client.get_controller_info(controller).await {
+
+                    Ok(info) =>{
+
+                        println!("Ok controller info ");
+                        let short_name = info.short_name.into_string().unwrap();
+                        let name = info.name.into_string().unwrap();
+                        tx.send(ShortbtData{
+                            address: info.address,
+                            short_name: short_name.clone(),
+                            bluetooth_version: info.bluetooth_version,
+                            name: name.clone()
+                        });
+                       return ShortbtData{
+                        address: info.address,
+                        short_name: short_name,
+                        bluetooth_version: info.bluetooth_version,
+                        name: name
+                    }
+                    },
+                    Err(e) => {
+                        println!("error controller info un wrap :{}", e.to_string());
+                        let null =  String::from("null");
+                tx.send(ShortbtData{
+                    address: Address::new([0,0,0,0,0,0]),
+                    name: null.clone(),
+                    short_name:null.clone(),
+                    bluetooth_version: 0,
+                });
+                        return ShortbtData{
+                            address: Address::new([0,0,0,0,0,0]),
+                            name: null.clone(),
+                            short_name: null,
+                            bluetooth_version: 0,
+                        }
+                    
+                    }
+             };
+            }
+            sleep(Duration::from_millis(1400)).await;
         }
     });
-    let mut  corte = false;
+    let  corte = false;
+
     while !corte {
-        let received= rx.try_recv().unwrap_or_else(|_e|  {
-            "_\n".to_string()}).replace("\n","");
+
+        let null =  String::from("null");
+        let received= rx.try_recv().unwrap_or_else(|_e | { return  ShortbtData {
+            address: Address::new([0,0,0,0,0,0]),
+            name: null.clone(),
+            short_name: null,
+            bluetooth_version: 0,
+        } });
         thread::sleep(time::Duration::from_secs(3));
-        println!("Got:{}, and comparation : {}", &received, received == "corte");
-        if received == "corte" {
-            corte=true;
-        }
+        println!("Got:{}", received);
+    
     }
 }
