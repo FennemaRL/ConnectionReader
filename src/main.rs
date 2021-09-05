d5eef6336f73b33e92f2d6d91608ea8dfa758b46
Fennema lucas <ld.fennema@gmail.com>
2021-09-05 13:32:28 -0300

    test bluer

--- src/main.rs
+++ src/main.rs
@@ -1,97 +1,139 @@
-use core::time;
-use std::time::Duration;
-use std::{fmt, io};
-use std::sync::mpsc;
-use std::thread;
-
-use bluez::Address;
-use bluez::client::BlueZClient;
-use tokio::time::sleep;
 
 
-struct ShortbtData {
-    pub name: String,
-    pub short_name: String,
 
-    pub address: Address,
-    pub bluetooth_version: u8,
+
+
+use bluer::{Adapter, AdapterEvent, Address, DeviceEvent};
+use futures::{pin_mut, stream::SelectAll, StreamExt};
+use std::env;
+
+async fn query_device(adapter: &Adapter, addr: Address) -> bluer::Result<()> {
+    let device = adapter.device(addr)?;
+    println!("    Address type:       {}", device.address_type().await?);
+    println!("    Name:               {:?}", device.name().await?);
+    println!("    Icon:               {:?}", device.icon().await?);
+    println!("    Class:              {:?}", device.class().await?);
+    println!("    UUIDs:              {:?}", device.uuids().await?.unwrap_or_default());
+    println!("    Paried:             {:?}", device.is_paired().await?);
+    println!("    Connected:          {:?}", device.is_connected().await?);
+    println!("    Trusted:            {:?}", device.is_trusted().await?);
+    println!("    Modalias:           {:?}", device.modalias().await?);
+    println!("    RSSI:               {:?}", device.rssi().await?);
+    println!("    TX power:           {:?}", device.tx_power().await?);
+    println!("    Manufacturer data:  {:?}", device.manufacturer_data().await?);
+    println!("    Service data:       {:?}", device.service_data().await?);
+    Ok(())
 }
 
-impl fmt::Display for ShortbtData {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "(name: {}, short_name: {}, address: {},bluetooth_version: {})", &self.name, &self.short_name, self.address, self.bluetooth_version)
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> bluer::Result<()> {
+    let with_changes = env::args().any(|arg| arg == "--changes");
+
+
+    let session = bluer::Session::new().await?;
+    let adapter_names = session.adapter_names().await?;
+    let adapter_name = adapter_names.first().expect("No Bluetooth adapter present");
+    println!("Discovering devices using Bluetooth adapater {}\n", &adapter_name);
+    let adapter = session.adapter(adapter_name)?;
+    adapter.set_powered(true).await?;
+
+    let device_events = adapter.discover_devices().await?;
+    pin_mut!(device_events);
+
+    let mut all_change_events = SelectAll::new();
+
+    loop {
+        tokio::select! {
+            Some(device_event) = device_events.next() => {
+                match device_event {
+                    AdapterEvent::DeviceAdded(addr) => {
+                        println!("Device added: {}", addr);
+                        if let Err(err) = query_device(&adapter, addr).await {
+                            println!("    Error: {}", &err);
+                        }
+
+                        if with_changes {
+                            let device = adapter.device(addr)?;
+                            let change_events = device.events().await?.map(move |evt| (addr, evt));
+                            all_change_events.push(change_events);
+                        }
+                    }
+                    AdapterEvent::DeviceRemoved(addr) => {
+                        println!("Device removed: {}", addr);
+                    }
+                    _ => (),
+                }
+                println!();
+            }
+            Some((addr, DeviceEvent::PropertyChanged(property))) = all_change_events.next() => {
+                println!("Device changed: {}", addr);
+                println!("    {:?}", property);
+            }
+            else => break
+        }
     }
+
+    Ok(())
 }
+/*
+
+use core::time;
+use std::{sync::mpsc, thread};
+
+async fn query_device(adapter: &Adapter, addr: Address) -> bluer::Result<()> {
+    let device = adapter.device(addr)?;
+    println!("    Address type:       {}", device.address_type().await?);
+    println!("    Name:               {:?}", device.name().await?);
+    println!("    Icon:               {:?}", device.icon().await?);
+    println!("    Class:              {:?}", device.class().await?);
+    println!("    UUIDs:              {:?}", device.uuids().await?.unwrap_or_default());
+    println!("    Paried:             {:?}", device.is_paired().await?);
+    println!("    Connected:          {:?}", device.is_connected().await?);
+    println!("    Trusted:            {:?}", device.is_trusted().await?);
+    println!("    Modalias:           {:?}", device.modalias().await?);
+    println!("    RSSI:               {:?}", device.rssi().await?);
+    println!("    TX power:           {:?}", device.tx_power().await?);
+    println!("    Manufacturer data:  {:?}", device.manufacturer_data().await?);
+    println!("    Service data:       {:?}", device.service_data().await?);
+    Ok(())
+}
+
 #[tokio::main]
 async fn main() {
     let (tx, rx) = mpsc::channel();
     tokio::spawn(async move {
-
-        let mut client = BlueZClient::new().unwrap();
+        let session = bluer::Session::new().await.unwrap();
+        let adapter_names = session.adapter_names().await.unwrap();
+        let adapter_name = adapter_names.first().expect("No Bluetooth adapter present");
+        let adapter = session.adapter(adapter_name).unwrap();
+        let _t = adapter.set_powered(true).await;
+       
         loop  {
-            let controllers =match client.get_controller_list().await   {
-                Ok(res) => res,
-                _ => Vec::new()
-            };
-
-            println!("Ok controller info :{:#?}",&controllers);
-            for controller in controllers {
-
-                match client.get_controller_info(controller).await {
-
-                    Ok(info) =>{
-
-                        println!("Ok controller info ");
-                        let short_name = info.short_name.into_string().unwrap();
-                        let name = info.name.into_string().unwrap();
-                        tx.send(ShortbtData{
-                            address: info.address,
-                            short_name: short_name.clone(),
-                            bluetooth_version: info.bluetooth_version,
-                            name: name.clone()
-                        });
-                       return ShortbtData{
-                        address: info.address,
-                        short_name: short_name,
-                        bluetooth_version: info.bluetooth_version,
-                        name: name
+            adapter.discover_devices().and_then(|devicesStream | {
+                devicesStream.for_each(|device| async{
+                    if let AdapterEvent:: DeviceAdded(addr) = device {
+                        query_device(&adapter, addr).await;
                     }
-                    },
-                    Err(e) => {
-                        println!("error controller info un wrap :{}", e.to_string());
-                        let null =  String::from("null");
-                tx.send(ShortbtData{
-                    address: Address::new([0,0,0,0,0,0]),
-                    name: null.clone(),
-                    short_name:null.clone(),
-                    bluetooth_version: 0,
-                });
-                        return ShortbtData{
-                            address: Address::new([0,0,0,0,0,0]),
-                            name: null.clone(),
-                            short_name: null,
-                            bluetooth_version: 0,
-                        }
-                    
-                    }
-             };
+
+                })
+            });
+
+                     //   {
+                     //       if let AdapterEvent::DeviceAdded(addr) = device {
+                     //           query_device(&adapter,addr).await;
+                     //           tx.send(1).and_then(|_opp| Ok(device));
+                     //       }   
+                     //   }
             }
-            sleep(Duration::from_millis(1400)).await;
-        }
     });
-    let  corte = false;
-
-    while !corte {
-
-        let null =  String::from("null");
-        let received= rx.try_recv().unwrap_or_else(|_e | { return  ShortbtData {
-            address: Address::new([0,0,0,0,0,0]),
-            name: null.clone(),
-            short_name: null,
-            bluetooth_version: 0,
-        } });
-        thread::sleep(time::Duration::from_secs(3));
-        println!("Got:{}", received);
-    
-    }
-}
+    //let  corte = false;
+//
+    //while !corte {
+//
+    //    let null =  String::from("null");
+    //    let received= rx.try_recv().unwrap_or_else(|e|  2);
+    //    thread::sleep(time::Duration::from_secs(3));
+    //    println!("Got:{}", received);
+    //
+    //}
+}*/
